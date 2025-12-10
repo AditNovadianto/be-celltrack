f9bc952392695c1e8bd21fd6cef6e8b750b460c5
AditNovadianto <aditya.novadianto@gmail.com>
2025-12-10 17:41:29 +0700

    init project

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { db } from "./config/db.js";
+// import authRoute from "./routes/authRoute.js";
+// import ProductRoute from "./routes/productRoute.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+// Test database connection
+async function testDBConnection() {
+  try {
+    await db.query("SELECT 1");
+    console.log("✅ Database connected successfully!");
+    return true;
+  } catch (error) {
+    console.error("❌ Failed to connect to the database:", error.message);
+    return false;
+  }
+}
+
+testDBConnection();
+//
+
+// app.use(authRoute);
+// app.use(ProductRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
